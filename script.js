1105eddae8a579131325cd508ca3e5003294f654
LaLunaInSky <moonaflordapele@gmail.com>
2025-03-06 12:42:40 -0300

    06/03/2025 12:42

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world!")
+"use strict"
+
+//senhaConfirme
+//senha
+
+const inputSenhas = [
+    document.querySelector("#senha"),
+    document.querySelector("#senhaConfirme")
+]
+
+inputSenhas.map((input)=>{
+    const imageEye = input.nextSibling.nextSibling
+    imageEye.addEventListener("click", ()=>{
+        if (imageEye.getAttribute("src") == "./icons/eye-slash.svg") {
+            imageEye.setAttribute("src", "./icons/eye.svg")
+            input.setAttribute("type", "text")
+        } else {
+            imageEye.setAttribute("src", "./icons/eye-slash.svg")
+            input.setAttribute("type", "password")
+        }
+    })
+})
